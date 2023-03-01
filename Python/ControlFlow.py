fda05ab498e7e3234b1cdcf2b9f0f1041a80ab1e
Cindy Thai <99574378+CindCodes@users.noreply.github.com>
2023-02-28 22:22:46 -0500

    Update ControlFlow.py

--- Python/ControlFlow.py
+++ Python/ControlFlow.py
@@ -54,7 +54,7 @@ print(divisible_by_ten(25))
 # Create a function named not_sum_to_ten() that has two parameters named num1 and num2.
 # Return True if num1 and num2 do not sum to 10. Return False otherwise.
 
-ef not_sum_to_ten(num1, num2):
+def not_sum_to_ten(num1, num2):
   if (num1+num2) != 10:
     return True
   else:
@@ -63,3 +63,78 @@ ef not_sum_to_ten(num1, num2):
 print(not_sum_to_ten(9, -1))
 print(not_sum_to_ten(9, 1))
 print(not_sum_to_ten(5,5))
+
+# In Range
+# Create a function named in_range() that has three parameters named num, lower, and upper.
+# The function should return True if num is greater than or equal to lower and less than or equal to upper. Otherwise, return False.
+
+def in_range(num, lower, upper):
+  if num >= lower and num <= upper:
+    return True
+  else:
+    return False
+
+print(in_range(10, 10, 10))
+print(in_range(5, 10, 20))
+
+# Same Name
+# Create a function named same_name() that has two parameters named your_name and my_name.
+# If our names are identical, return True. Otherwise, return False.
+
+def same_name(your_name, my_name):
+  if your_name == my_name:
+    return True
+  else:
+    return False
+
+print(same_name("Colby", "Colby"))
+print(same_name("Tina", "Amber"))
+
+# Always False
+# Create a function named always_false() that has one parameter named num.
+# Using an if statement, your variable num, and the operators >, and <, make it so your function will return False no matter what number is stored in num.
+
+def always_false(num):
+  if num > num and num < num:
+    return True
+  else:
+    return False
+
+print(always_false(0))
+print(always_false(-1))
+print(always_false(1))
+
+# Movie Review
+# Create a function named movie_review() that has one parameter named rating.
+# If rating is less than or equal to 5, return "Avoid at all costs!". If rating is between 5 and 9, return "This one was fun.". 
+# If rating is 9 or above, return "Outstanding!"
+
+def movie_review(rating):
+  if rating <= 5:
+    return "Avoid at all costs!"
+  elif rating < 9:
+    return "This one was fun."
+  return "Outstanding!"
+
+print(movie_review(9))
+print(movie_review(4))
+print(movie_review(6))
+
+# Max Number
+# Create a function called max_num() that has three parameters named num1, num2, and num3.
+# The function should return the largest of these three numbers. If any of two numbers tie as the largest, you should return "It's a tie!".
+
+def max_num(num1, num2, num3):
+  if num1 > num2 and num1 > num3:
+    return num1
+  elif num2 > num1 and num2 > num3:
+    return num2
+  elif  num3 > num1 and num3 > num2:
+    return num3
+  else:
+    return "It's a tie!"
+
+print(max_num(-10, 0, 10))
+print(max_num(-10, 5, -30))
+print(max_num(-5, -10, -10))
+print(max_num(2, 3, 3))
